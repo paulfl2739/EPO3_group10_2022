d6d8ea6a2d7e1ad4a87e6d3fca26fc7f24107894
paulfl2739 <P.F.Lee@student.tudelft.nl>
2022-12-02 16:45:02 +0100

    Create rom.vhdl

---
+++ rom.vhdl
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use iee.numeric_std.all;
+
+entity rom is
+    port(address : in  std_logic_vector(1 downto 0);
+         data    : out std_logic_vector(15 downto 0));
+end ram;
+
+architecture behaviour of rom is
+begin
+    with address select data <=
+        "0010001000001000" when "000", -- ctrl3 register
+        "0010000001001111" when "001", -- ctrl1 register
+        "0101001000000000" when others; -- xhigh register
+end behaviour;
