d2c7b7f78eccd492216a428d04950135c2bc838f
tjgeschiere <thijsgeschiere@hotmail.com>
2022-12-16 13:52:14 +0100

    fixed port names

--- L3GD20H driver.vhd
+++ L3GD20H driver.vhd
@@ -35,7 +35,7 @@ component driver is
 		spc		:	out	std_logic;	--max 10MHz 
 		cs		:	out	std_logic;	--when low, data is transmitted
 		gyro_data	:	out	std_logic_vector(7 downto 0); --neat vector containing angular data
-		data_ready	:	out	std_logic; --pulled high whenever gyro_data is correctly written
+		communicated	:	out	std_logic; --pulled high whenever gyro_data is correctly written
 		prev_spc_clk	:	out	std_logic
 	);
 end component driver;
@@ -63,7 +63,7 @@ component spi_data_controller is
 		reset		:	in	std_logic;
 		start_switch	:	in	std_logic;
 		
-		data_ready	:	in	std_logic; --output from spi driver when new data can be sent/read
+		communicated	:	in	std_logic; --output from spi driver when new data can be sent/read
 		drdy		:	in	std_logic; --output from gyro chip, when chip has calculated new data this pin is pulled high.
 
 		enable		:	out	std_logic;	--to turn on/off the drive
@@ -71,13 +71,13 @@ component spi_data_controller is
 	);
 end component spi_data_controller;
 
-signal enable, spc_clk_out, prev_spc_clk, data_ready_intern:							std_logic;
+signal enable, spc_clk_out, prev_spc_clk, communicated_intern:							std_logic;
 signal address:													std_logic_vector(2 downto 0);
 signal sdi_in:													std_logic_vector(15 downto 0);
 
 begin
 
-data_ready <= data_ready_intern;
+data_ready <= communicated_intern;
 
 lb1: spc_clockgen port map(
 		clk		=>	clk,
@@ -97,7 +97,7 @@ lb2: driver port map(
 		spc		=>		spc,
 		cs		=>		cs,
 		gyro_data	=>		gyro_data,
-		data_ready	=>		data_ready_intern,
+		communicated	=>		communicated_intern,
 		prev_spc_clk	=>		prev_spc_clk
 		);
 
@@ -111,7 +111,7 @@ lb7: spi_data_controller port map(
 			reset		=>		reset,
 			start_switch	=>		start_switch,
 			
-			data_ready	=>		data_ready_intern,
+			communicated	=>		communicated_intern,
 			drdy		=>		drdy,
 
 			enable		=>		enable,
