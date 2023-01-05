87fcec46f6728a7f9a27ac6b60bb019bb70eef47
Rowan Richie <45150261+RowanRichie@users.noreply.github.com>
2023-01-05 12:03:15 +0100

    Update char_location-behaviour.vhd
    Added platform collision and jump behaviour of the character when it does so

--- char_location-behaviour.vhd
+++ char_location-behaviour.vhd
@@ -21,6 +21,11 @@ signal y_pos : integer;
 signal V_y_1000: integer := 16000;
 signal V_y : integer;
 
+-- Grid coordinates for the character location (used for platform collision)
+signal character_column : integer;
+signal character_row : integer;
+signal platform_vector_X : std_logic_vector(31 downto 0);
+
 constant start_x : integer := 240;
 constant start_y : integer := 100;
 constant gravity : integer := 375;
@@ -49,6 +54,29 @@ process(vsync)
 begin
 if(rising_edge(vsync)) then  -- Signal from VGA that denotes whether a frame has been successfully rendered
 	
+	-- Divide x_pos by 20 to find out what column the character is in 
+	character_column <= x_pos / 20;
+	
+	-- Select the proper platform vector column
+	case character_column is
+		when 0 => platform_vector_X <= platform_vector(255 downto 224);
+		when 1 => platform_vector_X <= platform_vector(223 downto 192);
+		when 2 => platform_vector_X <= platform_vector(191 downto 160);
+		when 3 => platform_vector_X <= platform_vector(159 downto 128);
+		when 4 => platform_vector_X <= platform_vector(127 downto 96);
+		when 5 => platform_vector_X <= platform_vector(95 downto 64);
+		when 6 => platform_vector_X <= platform_vector(63 downto 32);
+		when 7 => platform_vector_X <= platform_vector(31 downto 1);
+		when others => platform_vector_X <= (others => '0');
+
+	-- Divide y_pos by 40 to find out what row the character is in
+	character_row <= y_pos / 40;
+	
+	-- If there is a platform, increase V_y back to 16000 and let it decrease from there again
+	if(platform_vector_X(character_row - 1) = '1') then
+		V_y_1000 <= 16000;  -- Character will initiate jump
+	end if;
+		
 	-- If character hits a platform, then we need to start from a higher y_velocity. We need to make it 160000 everytime it jumps.
 	V_y_1000 <= V_y_1000 - gravity;
 	V_y <= V_y_1000/1000;
