f00998be1e5e4d98258efa08837f7b2695dc79b3
FrederikSchiepers <78723436+FrederikSchiepers@users.noreply.github.com>
2023-01-06 13:27:46 +0100

    Update time_base.vhdl

--- time_base.vhdl
+++ time_base.vhdl
@@ -7,7 +7,7 @@ entity timebase is
 	port (	clk		: in	std_logic;
 		reset		: in	std_logic;
 
-		count_out	: out	std_logic_vector (24 downto 0)
+		count_out	: out	std_logic;
 	);
 end entity timebase;
 
