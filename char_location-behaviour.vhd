0272e6993e1ea8eed38c724bb47f28db05be707a
Rowan Richie <45150261+RowanRichie@users.noreply.github.com>
2023-01-06 12:40:48 +0100

    Update char_location-behaviour.vhd

--- char_location-behaviour.vhd
+++ char_location-behaviour.vhd
@@ -13,7 +13,6 @@ architecture behaviour of char_location is
 --	y	: out integer);
 --end component char_location;
 
-
 signal x_new : integer;
 signal y_new : integer;
 signal x_pos : integer;
@@ -33,12 +32,11 @@ constant gravity : integer := 375;
 -- Logic for when character goes all the way to right or left of the screen
 -- Character reaches top of the screen
 
-
 begin
 process(clk)
 begin
 if (rising_edge(clk)) then
-	if(start = '1') then 
+	if(start = '1') then -- A game starts, here we initiate all necessary values
 		x_pos <= start_x;
 		y_pos <= start_y;
 	elsif (vsync = '1') then
@@ -77,7 +75,7 @@ if(rising_edge(vsync)) then  -- Signal from VGA that denotes whether a frame has
 		V_y_1000 <= 16000;  -- Character will initiate jump
 	end if;
 		
-	-- If character hits a platform, then we need to start from a higher y_velocity. We need to make it 160000 everytime it jumps.
+	-- If character hits a platform, then we need to start from a higher y_velocity. We need to make it 16000 everytime it jumps.
 	V_y_1000 <= V_y_1000 - gravity;
 	V_y <= V_y_1000/1000;
 	
@@ -96,6 +94,14 @@ if(rising_edge(vsync)) then  -- Signal from VGA that denotes whether a frame has
 	else
 		x_new <= x_new + to_integer(velocity_x);
 	end if;
+		
+	if (y_pos = '0') then
+		death <= '1';
+	elsif (y_pos = '620') then
+		V_y <= 0;
+		V_y_1000 <= 0;
+	end if;
+		
 	y_new <= y_new + V_y;
 	
 else
@@ -112,6 +118,3 @@ end process;
 --	vsync => vsync);
 
 end behaviour;
-
-
-	
