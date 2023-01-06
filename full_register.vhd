b2e3308ecae1492945a48abb9e0c4a7b21d9d18a
Rowan Richie <45150261+RowanRichie@users.noreply.github.com>
2023-01-06 14:41:13 +0100

    Update full_register.vhd

--- full_register.vhd
+++ full_register.vhd
@@ -4,8 +4,10 @@ use IEEE.std_logic_1164.ALL;
 entity full_register is
    port(clk    : in  std_logic;
         reset  : in  std_logic;
-	inputt : in std_logic_vector(255 downto 0);
+	--inputt : in std_logic_vector(255 downto 0);
         start   : in  std_logic;
+	counter : in std_logic;
+	player_score : out integer;
         lfsr : out std_logic_vector(255 downto 0));
 end full_register;
 
