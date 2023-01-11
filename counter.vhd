313cbb01b74dd2b32636e3305f2a1d51e797c9c6
FrederikSchiepers <78723436+FrederikSchiepers@users.noreply.github.com>
2023-01-11 15:13:01 +0100

    Update counter.vhd

--- counter.vhd
+++ counter.vhd
@@ -5,5 +5,6 @@ entity counter is
    port(clk       : in  std_logic;
         reset     : in  std_logic;
         count_out : out std_logic_vector(18 downto 0));
+        pixel_count: out integer;
 end counter;
 
