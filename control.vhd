09b1dcbfd757552aa9d2ab4f2948bc89f7191ffe
FrederikSchiepers <78723436+FrederikSchiepers@users.noreply.github.com>
2023-01-12 12:05:06 +0100

    Update control.vhd

--- control.vhd
+++ control.vhd
@@ -6,13 +6,9 @@ entity control is
         clk           : in  std_logic;
         reset         : in  std_logic;
         v_sync_clk    : in  std_logic;
-        x             : in integer;
-        y             : in integer;
-	platform_grid : in  std_logic_vector(255 downto 0);		
-	x_out		: out integer;
-	y_out		: out integer;
-	platform_grid_out : out std_logic_vector(255 downto 0);
-        v_sync_clk_out    : out std_logic;
+        x             : out integer;
+        y             : out integer;		
+	platform_grid : out std_logic_vector(255 downto 0);
 	death		: out std_logic); 
 end control;
 
