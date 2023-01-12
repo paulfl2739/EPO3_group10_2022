4c5ca2fbc4134029ad4e15795643484c7ebd69be
FrederikSchiepers <78723436+FrederikSchiepers@users.noreply.github.com>
2023-01-12 10:49:44 +0100

    Update counter.vhd

--- counter.vhd
+++ counter.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.ALL;
 entity counter is
    port(clk       : in  std_logic;
         reset     : in  std_logic;
-        count_out : out std_logic_vector(18 downto 0));
-        pixel_count_out : out integer;
+        count_out : out std_logic_vector(18 downto 0);
+        pixel_count_out : out std_logic_vector(4 downto 0));
 end counter;
 
