a401c9d02d0f7242ea3d55e82c828c7fe864fdf0
FrederikSchiepers <78723436+FrederikSchiepers@users.noreply.github.com>
2023-01-15 18:01:35 +0100

    Update counter-behaviour.vhd

--- Game/counter-behaviour.vhd
+++ Game/counter-behaviour.vhd
@@ -6,6 +6,7 @@ use IEEE.numeric_std.ALL;
 
 architecture behavioral of counter is
     signal counter : unsigned(24 downto 0) := (others => '0');
+    --signal frame_counter : unsigned(18 downto 0) := (others => '0');
 begin
     process(clk)
     begin
@@ -14,6 +15,12 @@ begin
             			if (counter = 24_999_999) then
                 				update <= '1';
                 				counter <= (others => '0');
+						--if (frame_counter = 416_667) then
+							--new_frame = '1';
+							--frame_counter <= (others => '0');
+						--else
+							--new_frame <= '0';
+							--frame_counter <=
             			else
                 				update <= '0';
                 				counter <= counter + 1;
