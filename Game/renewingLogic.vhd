3e79e78283a552c70397757315750f6d6c21e014
paulfl2739 <P.F.Lee@student.tudelft.nl>
2023-01-20 10:08:29 +0100

    made determine_position entity
    caculates xpos and ypos based on block diagrams from Bas Verdoes

--- Game/renewingLogic.vhd
+++ Game/renewingLogic.vhd
@@ -5,69 +5,82 @@ use ieee.numeric_std.all;
 entity determine_position is
     port(   clk         : in std_logic; --25 MHz internal 
             reset       : in std_logic;
-            start       : in std_logic; 
-            enable      : in std_logic; -- '1' when frame has passed
-            collision   : in std_logic; -- from VGA
-            xvelocity   : in std_logic_vector(7 downto 0);
 
-            death       : out std_logic;
+            frame_passed: in std_logic; -- '1' when frame has passed for 1 clock cycle
+            collision   : in std_logic; -- from VGA, duration not very important
+            x_velocity   : in std_logic_vector(7 downto 0); -- from SPI
+
             x_position  : out std_logic_vector(8 downto 0);
             y_position  : out std_logic_vector(9 downto 0));
 end entity determine_position;
 
 architecture behavioral of determine_position is
 
-    signal next_x, current_x : std_logic_vector(8 downto 0);
-    signal nxt_y_pos, y_pos, integer;
-    signal nxt_y_velo, y_velo : integer;
+    signal current_y_vel, next_y_vel1, next_y_vel2, sum_y_vel : signed;
+    signal current_y_pos, next_y_pos, sum_y_pos: signed;
 
-    signal next_x_pos, current_x_pos, sum_x_pos, loop_sum_x_pos: signed;
+    signal next_x_pos1, next_x_pos2, current_x_pos, sum_x_pos, overflow_sum_x_pos: signed;
 
     begin
-process(clk, collision)
-    if rising_edge(clk) then
-        if reset then
-        y_pos <= 40;
-        nxt_ypos <= 40;
-        y_velo <= 0;
-        nxt_y_velo <= 0;
+
+sum_y_vel <= current_y_vel -375; -- '-375' is 'gravitational' acceleration
+next_y_vel1 <= sum_y_vel when (frame_passed = '1') else current_y_vel;
+next_y_vel2 <= (1500) when (collision = '1') else next_y_vel1;
+
+y_velocity_latch: process(clk, reset, next_y_vel)
+begin
+    if(rising_edge(clk)) then
+        if(reset = '1') then
+            current_y_vel <= 0;
         else
-        ypos <= nxt_y_pos;
-        y_velo <= nxt_y_velo;
+            current_y_vel <= next_y_vel2;
         end if;
     end if;
-end procces;
+end process;
 
-process(clk, collision)
-nxt_y_pos <= y_pos - y_velo;
+sum_y_pos <= current_y_pos + current_y_vel;
+next_y_pos <= sum_y_pos when (frame_passed = '1') else current_y_pos;
 
-if collision = '1' then
-nxt_y_velo <= 1500;
-else
-nxt_y_velo <= y_velo - gravity;
-end if;
-end procces;
+y_position_latch: process(clk, reset, next_y_pos)
+begin
+    if(rising_edge(clk)) then
+        if(reset = '1') then
+            current_y_pos <= 300; -- start position
+        else
+            current_y_pos <= next_y_pos;
+        end if;
+    end if;
+end process;
+
+y_position <= std_logic_vector(current_y_pos);
+
+--------------------------
+-- Calculate x position --
+--------------------------
 
 
-x_position <= current_x
 
+
+
+sum_x_pos <= x_velocity + current_x_pos;
+
+overflow_sum_x <= sum_x_pos-479 when sum_x_pos > 479 else sum_x_pos+479; -- overflow_sum_x is position of x when looped round the screen
+
+next_x_pos1 <= overflow_sum_x when (sum_x_pos > 479 or sum_x_pos < 0) else sum_x_pos;
+next_x_pos2 <= next_x_pos1 when (frame_passed = '1') else current_x_pos;
+    
 x_position_latch: process(clk, reset, next_x)
 begin 
     if(rising_edge(clk)) then
         if(reset = '1') then
             current_x_pos <= 240; -- reset x_position
         else
-            next_x_pos <= next_x_pos
+            current_x_pos <= next_x_pos2
         end if;
     end if;
 end process;
 
-sum_x_pos <= x_velocity + current_x_pos;
+x_position <= std_logic_vector(current_x_pos);
 
-loop_sum_x <= sum_x_pos-479 when sum_x_pos > 479 else sum_x_pos+479; -- loop_sum_x is position of x when looped round the screen
-
-next_x_pos <= loop_sum_x when (sum_x_pos > 479 or sum_x_pos < 0) else sum_x_pos;
-
-        
 
 end architecture behavioral;
